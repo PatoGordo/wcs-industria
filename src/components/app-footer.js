06f1f9e10132628546e12967457b07dce93e8272
PatoGordo <icariusv@gmail.com>
2022-02-05 14:52:02 -0300

    resolvendo problema com o footer

--- src/components/app-footer.js
+++ src/components/app-footer.js
@@ -1,6 +1,6 @@
 export class AppFooter {
   template = `
-    <footer class="app-footer">
+    <footer class="app-footer" v-if="$route.name === 'home'">
       <a href="https://github.com/patogordo/WCS-Industria">Acesse o código fonte do projeto.</a>
     </footer>
   `
